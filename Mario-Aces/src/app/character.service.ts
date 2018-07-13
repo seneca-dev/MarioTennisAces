477a7d2a9e765de537b5622a9fca3ae4893f28aa
seneca-dev <36150048+seneca-dev@users.noreply.github.com>
2018-07-13 16:03:47 -0400

    HttpModule
    Added the HttpModule and connection to an 'outside' datasource. Need to fix it checking for id instead of name for some reason.

--- Mario-Aces/src/app/character.service.ts
+++ Mario-Aces/src/app/character.service.ts
@@ -3,21 +3,36 @@ import { Character } from './character';
 import { CHARACTERS } from './mock-characters';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+
+  private charactersUrl = 'api/characters';
 
   getCharacters(): Observable<Character[]> {
-    this.messageService.add('CharacterService: fetched characters');
-    return of(CHARACTERS);
+    return this.http.get<Character[]>(this.charactersUrl).pipe(catchError(this.handleError('getCharacters', [])));
   }
 
   getCharacter(name: string): Observable<Character> {
-    this.messageService.add(`CharacterService: fetched character name=${name}`);
-    return of(CHARACTERS.find(character => character.name === name));
+    const url = `${this.charactersUrl}/${name}`;
+    return this.http.get<Character>(url).pipe(tap(_ => this.log(`fetched character name=${name}`)), catchError(this.handleError<Character>(`getCharacter name=${name}`)));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`CharacterService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    }
   }
 }
---
+++ Mario-Aces/src/app/in-memory-data.service.ts
@@ -0,0 +1,26 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const characters = [
+        { name: 'Mario', class: 'All-Around' },
+        { name: 'Luigi', class: 'All-Around' },
+        { name: 'Wario', class: 'Powerful' },
+        { name: 'Waluigi', class: 'Defensive' },
+        { name: 'Peach', class: 'Technical' },
+        { name: 'Daisy', class: 'All-Around' },
+        { name: 'Rosalina', class: 'Tricky' },
+        { name: 'Toad', class: 'Speedy' },
+        { name: 'Toadette', class: 'Technical' },
+        { name: 'Bowser', class: 'Powerful' },
+        { name: 'Bowser Jr.', class: 'Defensive' },
+        { name: 'Boo', class: 'Tricky' },
+        { name: 'Yoshi', class: 'Speedy' },
+        { name: 'Donkey Kong', class: 'Powerful' },
+        { name: 'Spike', class: 'Powerful' },
+        { name: 'Chain Chomp', class: 'Powerful' },
+        { name: 'Koopa', class: 'Speedy' }
+    ];
+    return { characters };
+  }
+}
